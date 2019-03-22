30cd756b4e16580f03b8140896902bb293eb0ceb
sephoro <eliassepuru@gmail.com>
2019-03-22 12:06:09 +0200

    Added the about file to my website

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ app.get('/', function (req, res) {
   res.send('Hello to Boikanyo, Elias and the rest of the World!')
 })
 
+app.get('/author', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
 app.listen(3000)
 console.log('Express server running on port 3000')
