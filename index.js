9a41f488e8f2546f4e3b47a4dbc77c5401067c8e
sephoro <eliassepuru@gmail.com>
2019-03-22 14:32:01 +0200

    Added routing as a module

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 'use strict'
 
-let path = require('path')
 let express = require('express')
 let app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello to Boikanyo, Elias and the rest of the World!')
-})
+const newLocal = './mainRoutes'
+let mainRouter = require(newLocal)
 
-app.get('/author', function (req, res) {
-  res.sendFile(path.join(__dirname, 'views', 'about.html'))
-})
+app.use(mainRouter)
 
 app.listen(3000)
 console.log('Express server running on port 3000')
---
+++ mainRoutes.js
@@ -0,0 +1,15 @@
+'use strict'
+
+let path = require('path')
+let express = require('express')
+let mainRouter = express.Router()
+
+mainRouter.get('/', function (req, res) {
+  res.send('Hello Boikanyo, Elias and the rest of the World!')
+})
+
+mainRouter.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+module.exports = mainRouter
